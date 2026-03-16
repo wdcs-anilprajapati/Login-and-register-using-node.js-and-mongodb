cd95c11bac5b630b42a1b8467f432ab45b2a3e50
Anil Prajapati <anil.prajapati@webcluesinfotech.com>
2026-03-16 10:48:06 +0530

    gome

--- server.js
+++ server.js
@@ -1,32 +1,40 @@
-const RegisterController = require('./Controllers/RegisterController')
-const express = require('express')
-const mongo = require('mongoose')
-const dotenv = require('dotenv')
-dotenv.config({ path: './vars/.env' })
-// const script = require('./Public/JS/script')
-const RegisterRouter = require('./Routes/RegisterRoute')
-const app = express()
+const RegisterController = require('./Controllers/RegisterController');
+const express = require('express');
+const mongo = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './vars/.env' });
+
+const RegisterRouter = require('./Routes/RegisterRoute');
+
+const app = express();
+
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// app.use(express.json());
+/* MongoDB Connection */
+/* Use container name "mongodb" instead of 127.0.0.1 */
+const dbURI = process.env.MONGO_URI || "mongodb://mongodb:27017/signup-login";
 
-const dbURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/signup-login";
-
-mongo.connect(dbURI)
-    .then(() => console.log('Connected to MongoDB @', dbURI))
-    .catch(err => {
-        console.error('Mongoose connection error:', err);
-        process.exit(1); // Exit with error so Docker knows it failed
-    });
-
-
-
-
-app.use(express.json())
-app.use('/App', RegisterRouter)
-app.use(express.static('./Public'))
-app.listen(2000, () => {
-    console.log('yes the server has been started');
-
-
+mongo.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
+.then(() => {
+    console.log("Connected to MongoDB @", dbURI);
+})
+.catch((err) => {
+    console.error("Mongoose connection error:", err);
+    process.exit(1);
+});
+
+/* Routes */
+app.use('/App', RegisterRouter);
+
+/* Static Files */
+app.use(express.static('./Public'));
+
+/* Start Server */
+app.listen(2000, () => {
+    console.log("Server started on port 2000");
+});
